89061cb7bc99911512c5f3afe3af0d53619d8e86
NITS\Nitish Kumar <nitishnoetic@gmail.com>
2017-04-27 18:12:18 +0530

    Home page prefixes fixed

--- src/System/Providers/NitsRoutesServiceProvider.php
+++ src/System/Providers/NitsRoutesServiceProvider.php
@@ -25,7 +25,7 @@ class NitsRoutesServiceProvider extends RouteServiceProvider
 
     protected function mapApiRoutes()
     {
-        Route::prefix('Nitseditor\api')
+        Route::prefix('api')
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(__DIR__ . '\..\Routes\api.php');
@@ -33,8 +33,7 @@ class NitsRoutesServiceProvider extends RouteServiceProvider
 
     protected function mapWebRoutes()
     {
-        Route::prefix('Nitseditor')
-            ->middleware('web')
+        Route::middleware('web')
             ->namespace($this->namespace)
             ->group(__DIR__ . '\..\Routes\web.php');
     }
