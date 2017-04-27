89e50f23de7e413106f3f45332eb3b2674d75193
Nitish Kumar <nitish1986@users.noreply.github.com>
2017-04-27 19:45:49 +0530

    Update config.php

--- src/System/config.php
+++ src/System/config.php
@@ -2,13 +2,6 @@
 
 return [
     'packages' => [
-        'blog'         => [
-            'name'             => 'Blog',
-            'description'      => 'NitsEditor Blog for Laravel 5.4',
-        ],
-        'shop'         => [
-            'name'             => 'Shop',
-            'description'      => 'NitsEditor Shopping for Laravel 5.4',
-        ],
+        //List of packages..
     ],
 ];
