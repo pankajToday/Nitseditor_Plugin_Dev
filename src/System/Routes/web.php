7d958246d526bdca2b776489acccadc4bcad863f
Nitish Kumar <nitishnoetic@gmail.com>
2018-03-16 23:11:20 +0530

    Some route changes intialisation: will be linking to the database

--- src/System/Routes/web.php
+++ src/System/Routes/web.php
@@ -1,4 +1,5 @@
 <?php
 
 
-Route::get('/{any}', ['as' => 'NitsEditor', 'uses' => 'NitsEditorController@index']);
+Route::get('/', ['as' => 'NitsEditor', 'uses' => 'NitsEditorController@index']);
+Route::get('/{any}', ['as' => 'NitsEditor', 'uses' => 'NitsEditorController@index'])->where('any', '.*');
